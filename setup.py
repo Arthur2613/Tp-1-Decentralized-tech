88ba8739a047d483f680ac1195ba6eb1cfdc836f
Arthur2613 <160055419+Arthur2613@users.noreply.github.com>
2025-01-13 14:01:10 +0100

    setup.py
    correction de setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 from distutils.core import setup
 setup(
- name='{name-of-your-project}',
+ name='{TD1 decentralized tech}',
  version='1.0',
- author='{your-full-name-or-your-github-username}',
+ author='{Arthur DA COSTA VIEIRA}',
  license='MIT',
  long_description=open('README.md').read(),
 )
